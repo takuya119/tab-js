639fb069ad22c3d9ae8e00332ac9e2dcda282211
takuya119 <takuya.nakahara.119@gmail.com>
2022-07-16 16:24:10 +0900

    リファクタリング

--- tab.js
+++ tab.js
@@ -4,6 +4,8 @@
   const $tab = $doc.getElementById('js-tab')
   const $nav = $tab.querySelectorAll('[data-nav]')
   const $content = $tab.querySelectorAll('[data-content]')
+  const ACTIVE_CLASS = 'is-active'
+  const navLen = $nav.length
   
   
   const init = () => {
@@ -17,18 +19,18 @@
     const targetVal = $this.dataset.nav
     
     let index = 0
-    while(index < $nav.length){
+    while(index < navLen){
       $content[index].style.display = 'none'
-      $nav[index].classList.remove('is-active')
+      $nav[index].classList.remove(ACTIVE_CLASS)
       index++
     }
 
     $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block'
-    $nav[targetVal].classList.add('is-active')
+    $nav[targetVal].classList.add(ACTIVE_CLASS)
   }
 
   let index = 0
-  while(index < $nav.length){
+  while(index < navLen){
     $nav[index].addEventListener('click', (e) => handleClick(e))
     index++
   }
